194337ae7f10bcc303d117bd9ec2862f3fd6775e
pranay <pranaytejareddy16@gmail.com>
2022-07-15 10:03:55 +0530

    first commit

---
+++ StudentPortfolio/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentPortfolio.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Column(TypeName="nvarchar(100)")]
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Location { get; set; }
+        public int Rank { get; set; }
+
+    }
+
+  
+}
---
+++ StudentPortfolio/Models/StudentDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortfolio.Models
+{
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext>options):base(options)        
+        {
+                
+        }
+
+        DbSet<Student> Students { get; set; }
+    }
+}
